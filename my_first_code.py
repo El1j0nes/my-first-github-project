50aa9bbbe752103a213972376ccfb5641456be1c
Eli Jones (s5712514) <s5712514@bournemouth.ac.uk>
2024-10-01 10:33:20 +0100

    inital message

---
+++ my_first_code.py
@@ -0,0 +1,18 @@
+
+
+
+print("my first python code")
+print(1 + 2)
+print("dear python i know how to display my text!") 
+print("world")
+print("hello", end="")
+print("world")
+print(f"{"eli"} is {18} years old")
+print(5022 - 901)
+print(5 * 10)
+print(604 / 4)
+print( 3 % 2)
+print( 9 // 3)
+print( 8 // 3)
+
+"git init"
